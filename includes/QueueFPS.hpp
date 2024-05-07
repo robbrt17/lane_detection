2cb1b3267519004ce4671369f0b3704c62cf709c
Robert Balan <robert.balan17@gmail.com>
2024-05-07 13:04:36 +0300

    QueueFPS class changes

--- includes/QueueFPS.hpp
+++ includes/QueueFPS.hpp
@@ -13,8 +13,6 @@
 template <typename T>
 class QueueFPS : public std::queue<T> {
     public:
-        
-
         QueueFPS();
 
         void push(const T& entry);
@@ -27,59 +25,5 @@ class QueueFPS : public std::queue<T> {
         cv::TickMeter tm;
         std::mutex mutex;
 };
-
-// template <typename T>
-// class QueueFPS : public std::queue<T>
-// {
-// public:
-//     QueueFPS()
-//     {
-//         counter = 0;
-//     }
-
-//     void push(const T& entry)
-//     {
-//         std::lock_guard<std::mutex> lock(mutex);
-//         std::queue<T>::push(entry);
-//         counter += 1;
-
-//         if (counter == 1)
-//         {
-//             tm.reset();
-//             tm.start();
-//         }
-//     }    
-
-//     T get()
-//     {
-//         std::lock_guard<std::mutex> lock(mutex);
-//         T entry = this->front();
-//         this->pop();
-//         return entry;
-//     }
-
-//     float getFPS()
-//     {
-//         tm.stop();
-//         double fps = counter / tm.getTimeSec();
-//         tm.start();
-//         return static_cast<float>(fps);
-//     }
-
-//     void clear()
-//     {
-//         std::lock_guard<std::mutex> lock(mutex);
-//         while (!this->empty())
-//         {
-//             this->pop();
-//         }
-//     }
-
-//     unsigned int counter;
-
-// private:
-//     cv::TickMeter tm;
-//     std::mutex mutex;
-// };
 
 #endif
